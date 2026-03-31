ab6d029b5f412d5898fd06f06a4f9b8cdc8cf590
LeonardoGaeta <leonardogaetaalves@gmail.com>
2026-03-31 12:53:24 -0300

    feat: add Header component and Tailwind CSS configuration

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
+import Header from "../../components/header/Header";
 
 
 function Home() {
     return (
         <div className="">
+            <Header />
+            AAAAAAAAAAA
         </div>
     )
 }

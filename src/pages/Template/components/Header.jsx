1941c2557b0ecef70b3d78cc7cd9c5828b2ed866
LeonardoGaeta <leonardogaetaalves@gmail.com>
2026-04-13 00:43:50 -0300

    feat: update Header component to conditionally show back button based on route

--- src/pages/Template/components/Header.jsx
+++ src/pages/Template/components/Header.jsx
@@ -1,14 +1,28 @@
 import ArrowIcon from '@assets/images/Vetor - Seta.svg?react';
 import imageUrl from '@assets/images/generic-logo-140952.png';
 
+import { useLocation, useNavigate } from 'react-router-dom';
+
 function Header({ returnable }) {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const showButton = returnable || location.pathname.startsWith('/word/');
+
     return (
         <div
             className="w-full h-24 flex items-center justify-center shadow-custom-b relative bg-(--bg) shrink-0 banana"
         >
             {/* Seta de voltar */}
-            {returnable && 
-            <div className="absolute left-10">
+            {showButton && 
+            <div
+              className="absolute left-10
+                transition-all duration-150
+                hover:scale-110
+                active:scale-100
+              "
+              onClick={() => navigate(-1)}
+            >
                 <ArrowIcon className="w-10 h-10 color-(--primary) cursor-pointer -scale-x-100" />
             </div>}
 

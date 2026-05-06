698c3c9e7b64197b923ab44f144b2ed15a791aa8
Leonardo <leonardogaetaalves@gmail.com>
2026-05-06 11:25:07 -0300

    feat: enhance GlossaryPage with sorted data display and add TranslatePage language swap functionality

--- src/pages/GlossaryPage/GlossaryPage.jsx
+++ src/pages/GlossaryPage/GlossaryPage.jsx
@@ -1,5 +1,7 @@
 import SubContent from "@template/components/SubContent";
 import { useWords } from '@contexts/WordsContext';
+import { useState } from "react";
+import GoIcon from "@assets/images/Vetor - Ir.svg?react"
 
 function GlossaryPage() {
     const { sortedData } = useWords();
@@ -8,13 +10,42 @@ function GlossaryPage() {
 
     return (
         <SubContent>
-            <div className="rounded-t-2xl shadow-custom-b w-[calc(100%+1rem)] py-3">
+            <div className="rounded-t-2xl shadow-custom-b w-[calc(100%+1rem)] py-3 rounded-b-2xl">
                 <div className="flex flex-col items-center justify-center">
                     <div className="w-fit text-center">
                         <p className="text-4xl text-(--secondary)">Glossário</p>
                         <div className="border-b-2 border-(--secondary) w-[50%] mx-auto" />
                     </div>
                 </div>
+            </div>
+            <div className="w-[calc(100%+1rem)]">
+                {sortedData.map((val, i) => {
+                        const currentWord = val.identification.name.en;
+                        const firstLetter = currentWord[0].toUpperCase();
+
+                        const isNewLetter = i === 0 || firstLetter !== sortedData[i - 1].identification.name.en[0].toUpperCase();
+
+                        return (
+                            <div className="flex flex-col w-full">
+                                {isNewLetter && (
+                                    <div className="text-center text-4xl text-(--secondary) py-4 shadow-custom-b">
+                                        {firstLetter}
+                                    </div>
+                                )}
+                                <div className="shadow-custom-b py-10 px-10 flex items-center justify-between">
+                                    <div>
+                                        <div className="flex items-end gap-2">
+                                            <p className="text-4xl border-0 text-(--text-topper)">{val.identification.name.en}</p>
+                                            <p className="text-(--black-transparent) capitalize">({val.data.classification})</p>
+                                        </div>
+                                        <p className="text-(--black-transparent)">{val.identification.name.pt[0]}</p>
+                                    </div>
+                                    <GoIcon className="text-(--black-transparent)" />
+                                </div>
+                            </div>
+                        );
+                    }
+                )}
             </div>
         </SubContent>
     )
--- src/pages/TranslatePage/TranslatePage.jsx
+++ src/pages/TranslatePage/TranslatePage.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 
 
 function TranslatePage() {
+    const [languageSwapped, setLanguageSwapped] = useState(false);
+    const [currentLanguage, setCurrentLanguage] = useState("en");
+
+    const swapLanguage = () => {
+        setLanguageSwapped((prev) => !prev);
+        setCurrentLanguage(() => {
+            return languageSwapped ? "pt" : "en";
+        });
+    }
+
+    // const traduzir = async (text) => {
+    //     try {
+    //         const response = await(fetch)
+    //     } catch (err) {
+
+    //     }
+    // }
+
     return (
         <div className="">
             EEEEEEE
